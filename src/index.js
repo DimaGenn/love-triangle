1ef85fca7fc3343caad21342cb0cf8b3a85fbf62
DimaGenn <44157969+DimaGenn@users.noreply.github.com>
2019-03-21 15:11:17 +0300

    This programm can find all love triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  let count = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    if (preferences[i] != i + 1) {
+      let j = preferences[i] - 1;
+      let k = preferences[j] - 1;
+
+      if (preferences[k] - 1 == i) {
+        count += 1;
+      }
+    }
+  }
+  count = count / 3;
+  return count;
 };
